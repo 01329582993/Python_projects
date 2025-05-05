dc433f619e43317f0878b131d5208ec3747c52af
LIMASSOU HAMAN BELLO <149891433+01329582993@users.noreply.github.com>
2025-05-05 16:55:48 +0600

    Add files via upload

---
+++ weird_number.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+if number % 2 == 0:
+    if number >= 2 and number <= 5:
+        print("Not Weird")
+    elif number >= 6 and number <= 20:
+        print("Weird")
+    elif number > 20:
+        print("Not Weird")
+else:
+    print("Weird")
